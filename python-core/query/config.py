66c78195e85386c396c502355c10d233e3563788
Sumit1642 <sumeetyadav1642@gmail.com>
2025-07-07 10:41:32 +0530

    Fix: Previously, the paths was set only for linux systems but now its relative to the project

--- python-core/query/config.py
+++ python-core/query/config.py
@@ -1,4 +1,4 @@
 # python-core/query/config.py
 CHROMA_COLLECTION_NAME = "file_embeddings"
 EMBED_MODEL_NAME = "all-MiniLM-L6-v2"
-SQLITE_DB_PATH = "/home/sumit/Desktop/NeoDex/rust-core/index.db"
+SQLITE_DB_PATH = "rust-core/index.db"
