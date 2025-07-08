b20a8bbe14ac0843fba54a5799b4a33c31d70756
Sumit1642 <sumeetyadav1642@gmail.com>
2025-07-08 11:25:35 +0530

    Probelm : The paths weren't being resolved , Fix : So i used the python's os module so that the python files can be ran from anywhere

--- python-core/query/config.py
+++ python-core/query/config.py
@@ -1,4 +1,9 @@
 # python-core/query/config.py
+import os
+
+# Get absolute path to rust-core/index.db regardless of where this is run
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SQLITE_DB_PATH = os.path.abspath(os.path.join(BASE_DIR, "../../rust-core/index.db"))
+
 CHROMA_COLLECTION_NAME = "file_embeddings"
 EMBED_MODEL_NAME = "all-MiniLM-L6-v2"
-SQLITE_DB_PATH = "rust-core/index.db"
